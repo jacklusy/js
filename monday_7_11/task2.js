e2d8b61e7c847bd0e5a700c1a8d7cedf7c312eef
jacklusy <jackalloussi23@gmail.com>
2022-11-07 06:56:38 +0200

    task

---
+++ monday_7_11/task2.js
@@ -0,0 +1,8 @@
+
+for (i = 1; i<=4; i++){
+    for (j=1; j<=i; j++){
+        for (a=1; a<=(j+1); a++){
+            console.log(a);
+        }
+    }
+}
---
+++ monday_7_11/task3.js
@@ -0,0 +1,8 @@
+for (a=0; a<=1000; a++){
+    if ( a % 13 == 0){
+        console.log(a);
+    }
+    else{
+        continue;
+    }
+}
